55b0e7d1114dcb18ee80c02fcfbdd689b907fee9
JohnBundalian <114544102+JohnBundalian@users.noreply.github.com>
2022-10-26 22:58:00 +1100

    Troubleshooting Level Character Stats

---
+++ Assets/Scripts by John Bundalian/StatModifier.cs
@@ -0,0 +1,14 @@
+namespace JohnBundalian
+{
+    public class StatModifier
+    {
+        public readonly float Value;
+
+        public StatModifier(float value)
+        {
+            value = value;
+        }
+    }
+}
+
+
